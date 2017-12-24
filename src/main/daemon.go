a948778220d0ed98b7affb3d9d108b71e24166ba
kongminhao <2015141463077@stu.scu.edu.cn>
2017-12-24 12:51:09 +0800

    complete

--- src/main/daemon.go
+++ src/main/daemon.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"net"
 	"network"
 	"runtime"
+	"fmt"
 )
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	node := network.Init_node()
-	test_addr, _ := net.ResolveTCPAddr("tcp", "192.168.0.12:7890")
-	go node.Announce_peer(123, *test_addr, "test.sh")
-	go node.Announce_peer(345, *test_addr, "kkkk.sh")
 	node.Init_rpc_server()
+	fmt.Println(node)
 }
